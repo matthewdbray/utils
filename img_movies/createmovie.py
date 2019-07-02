229e99f407f8bbe379527615371751cbb6067e35
matthew.d.bray <matthew.d.bray@erdc.army.mil>
2019-07-02 17:29:52 -0500

    Adding movie scripts

--- img_movies/createmovie.py
+++ img_movies/createmovie.py
@@ -14,28 +14,39 @@ def createmovie(full_name):
    files = glob.glob('*')
    jpgFiles = False
    tifFiles = False
+   pngFiles = False
    for f in files:
        ext = f.split('.')[-1]
        if ext == 'jpg':
            print "Found jpg file: %s" % f
            jpgFiles = True
-           tifFiles = False
            print "Already converted to jpg files"
            break
        elif ext == 'tif':
            tifFiles = True
+       elif ext == 'png': 
+           pngFiles = True
+
    if tifFiles:
        print "Converting from tif to jpg"
        convertTiff()
+       jpfFiles = True
 
    print "Creating mp4"                                                            
-   cmd = 'ffmpeg -framerate 10 -pattern_type glob -i "*.jpg" -c:v libx264 %s.mp4' % full_name 
-   failure, output = commands.getstatusoutput(cmd)                                 
-   print failure, output 
+   if jpgFiles:
+       cmd = 'ffmpeg -framerate 10 -pattern_type glob -i "*.jpg" -c:v libx264 %s.mp4' % full_name 
+       failure, output = commands.getstatusoutput(cmd)                                 
+       print failure, output 
                                                                                    
+   if pngFiles: 
+       cmd = 'ffmpeg -framerate 10 -pattern_type glob -i "*.png" -c:v libx264 %s.mp4' % full_name
+       failure, output = commands.getstatusoutput(cmd)
+       print failure, output
+   
    print "Creating wmv"                                                            
    cmd = 'ffmpeg -i %s.mp4 -c:v wmv2 -b:v 20M -s 720x720 -c:a wmav2 -b:a 192k %s.wmv' % (full_name, full_name) 
    failure, output = commands.getstatusoutput(cmd)                                 
+   print failure, output
                                                                                    
    print "--DONE--"                                                                
                                                                                    
