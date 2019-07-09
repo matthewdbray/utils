5ae90b08f2de33aeb738fbf6a57d5a98a860e082
matthew.d.bray <matthew.d.bray@erdc.army.mil>
2019-07-09 15:20:58 -0500

    Added functionality to keep material properties from 2dm in obj

--- create_dm/dm2d_to_obj.py
+++ create_dm/dm2d_to_obj.py
@@ -11,10 +11,17 @@ if __name__ == '__main__':
 
     parser.add_argument('--obj', '-o', action='store', required=True, 
             dest='obj', help='This is the name of the obj file.')
+    parser.add_argument('--mat', action='store_true', required=False,
+            dest='mat', help='This is if you want material.')
+
 
     results = parser.parse_args()
+
     two_dm = results.two_dm
     obj = results.obj
+    if results.mat:
+        mat = results.mat
+        mats = {}
 
     facets = []
     nodes = []
@@ -24,7 +31,12 @@ if __name__ == '__main__':
         for line in ifile.readlines(): 
             header = line.split()[0]
             if header == 'E3T': 
-                facets.append(line.split()[2:5])
+                if mat:
+                    facets.append(line.split()[2:6])
+                    mats[str(line.split()[-1])] = True
+                else:
+                    facets.append(line.split()[2:5])
+
             elif header == 'ND': 
                 nodes.append(line.split()[2:5])
             else: 
@@ -36,7 +48,19 @@ if __name__ == '__main__':
             nline = 'v ' + ' '.join(node) + '\n'
             ofile.write(nline)
         for face in facets: 
-            nline = 'f ' + ' '.join(face) + '\n'
-            ofile.write(nline)
+            if mat:
+#                print(mats['1'])
+                if mats[str(face[-1])] != False:
+                    mat_line = 'usemtl Mat_%s\n' % str(face[-1]) 
+                    ofile.write(mat_line)
+                    mats[str(face[-1])] = False
+                    nline = 'f ' + ' '.join(face[:-1]) + '\n'
+                    ofile.write(nline)
+                else:
+                    nline = 'f ' + ' '.join(face[:-1]) + '\n'
+                    ofile.write(nline)
+            else:
+                nline = 'f ' + ' '.join(face) + '\n'
+                ofile.write(nline)
 
 
